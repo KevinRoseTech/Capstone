41e588568490c3872b41cbda83449ec0948464f0
Kevin David <107796703+MarmotMoment@users.noreply.github.com>
2023-07-01 14:39:08 -0600

    Updates
    Added page template for archive list page, starting database integration

--- src/main/webapp/WEB-INF/classes/com/grocie/servlets/LoginServlet.java
+++ src/main/webapp/WEB-INF/classes/com/grocie/servlets/LoginServlet.java
@@ -1,29 +1,21 @@
-package com.grocie.servlets;
-
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.servlet.annotation.WebServlet;
+
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		PrintWriter writer = response.getWriter();
-		String htmlResponse = "<html>"
-				+ "<form><input type=text>Name</input></form></html>";
-		
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String email = request.getParameter("emailField");
+        String password = request.getParameter("passwordField");
+    }
 }
+}
+
+
+//TODO: Change to your personal database URL, username, password
